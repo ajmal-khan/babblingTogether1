559248f2223db741cb6374dddc6097fec3c51e5f
Macbook Pro Retina <macbookproretina@Ajmal-Khans-Macbook-Pro-Retina.local>
2015-06-18 14:10:12 +0200

    Initial Commit

---
+++ babblingTogether1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  babblingTogether1
+//
+//  Created by Macbook Pro Retina on 2015-06-18.
+//  Copyright (c) 2015 Codopedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ babblingTogether1Tests/babblingTogether1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  babblingTogether1Tests.swift
+//  babblingTogether1Tests
+//
+//  Created by Macbook Pro Retina on 2015-06-18.
+//  Copyright (c) 2015 Codopedia. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class babblingTogether1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
