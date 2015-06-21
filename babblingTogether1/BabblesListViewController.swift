46969f87b0defd4a046387a252011516b7970505
Macbook Pro Retina <macbookproretina@Ajmal-Khans-Macbook-Pro-Retina.local>
2015-06-22 00:04:25 +0200

    List of available babbles shown in textview now but not playable yet

--- babblingTogether1/BabblesListViewController.swift
+++ babblingTogether1/BabblesListViewController.swift
@@ -9,15 +9,48 @@
 import UIKit
 import Parse
 
-class BabblesListViewController: UIViewController {
+class BabblesListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    //All the sounds saved at parse.com are PFObjects.
+    var babblesArray : [PFObject] = [];
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self;
+        self.tableView.delegate = self;
     }
     
+    override func viewDidAppear(animated: Bool) {
+        
+        var babblesQuery = PFQuery(className: "Babble");
+        babblesQuery.whereKeyExists("Audio");
+        babblesQuery.includeKey("User");
+        
+        babblesQuery.findObjectsInBackgroundWithBlock {
+            (downLoadedBabbles, error) -> Void in
+            if error == nil{
+                self.babblesArray = downLoadedBabbles as! [PFObject]
+                self.tableView.reloadData();
+            }
+            else{
+                println("Sorrey, the list of babbles was not downloded from parse.com");
+            }
+        }
+    }//function viewDidAppear ends here.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.babblesArray.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var babble =  self.babblesArray[indexPath.row];
+        var babbler = babble["User"] as! PFUser;
+        var cell = UITableViewCell();
+        cell.textLabel!.text = babbler["username"] as? String;
+        return cell;
+    }
     
     @IBAction func buttonLogoutTapped(sender: AnyObject) {
 
---
+++ babblingTogether1/BabblesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  babblesTableViewCell.swift
+//  babblingTogether1
+//
+//  Created by Macbook Pro Retina on 2015-06-21.
+//  Copyright (c) 2015 Codopedia. All rights reserved.
+//
+
+import UIKit
+
+class babblesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}//function babblesTableViewCell ends here.
